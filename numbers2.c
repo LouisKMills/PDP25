5b01971c6e9ad21259a381bb514727ee6a86d082
LouisKPM <louiskmills@gmail.com>
2025-05-01 06:41:14 +0930

    implement worker-master logic in numbers2.c

--- numbers2.c
+++ numbers2.c
@@ -40,9 +40,17 @@ int main(int argc, char *argv[]) {
     1. Send my number to the right neighbour
 
     2. Receive the number from the left neighbour
-        set flag true/false indiciation 'out of order'
+        set flag true/false indiciation 'out of order'*/
 
-    3.
+    // MPI SEND AND RECEIVE
+    // Send my number to the right neighbour & receive it from my left
+    MPI_Sendrecv(
+        &myNumber, 1, MPI_INT, right, 0,
+        &leftNumber, 1, MPI_INT, left,  0,
+        MPI_COMM_WORLD, MPI_STATUS_IGNORE
+    );
+
+    /*3.
     If worker:
         send to master:
             rank ID
@@ -55,6 +63,26 @@ int main(int argc, char *argv[]) {
             Receive that worker's report
             If its flag is true, print the message
     */
+   if (rank != 0) {
+        /* 2. worker: decide out‐of‐order, then report to master */
+        int out_of_order = (myNumber < leftNumber) ? 1 : 0;
+        int info[2] = { myNumber, out_of_order };
+        MPI_Send(info, 2, MPI_INT, 0, 1, MPI_COMM_WORLD);
+    } else {
+        /* 3. master: collect reports in rank order, print results */
+        int total = 0;
+        for (int src = 1; src < size; ++src) {
+            int info[2];
+            MPI_Recv(info, 2, MPI_INT, src, 1, MPI_COMM_WORLD, &status);
+            if (info[1]) {
+                printf("Process %d has number %d out of order.\n", src, info[0]);
+                ++total;
+            }
+        }
+        printf("The number of processes holding an out-of-order number is %d.\n",
+            total);
+    }
+
 
     // This process is done.
     MPI_Finalize();
