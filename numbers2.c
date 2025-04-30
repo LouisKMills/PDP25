dcbc4266ae6689486fff3fb61f15fdea1c9d4b4f
LouisKPM <louiskmills@gmail.com>
2025-05-01 06:04:33 +0930

    reintroduce left/right logic to numbers2.c

--- numbers2.c
+++ numbers2.c
@@ -14,10 +14,13 @@
 int main(int argc, char *argv[]) {
     int rank, size; // rank: current process, size: # of processes
 
-    // hardcoded array of numbers for each process, each process will
+    // Hardcoded array of numbers for each process, each process will
     // use its rank to pull its number from the array
     int numbers[5] = {10, 20, 5, 15, 25};
     int myNumber; // this will hold the number for current process
+
+    // This will hold the value for left process
+    int leftNumber; // NOTE: will be received using MPI_Send and MPI_Recv
 
     // Initialize the MPI environment
     MPI_Init(&argc, &argv);
@@ -26,7 +29,11 @@ int main(int argc, char *argv[]) {
     MPI_Comm_size(MPI_COMM_WORLD, &size);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
+    // Determine the left and right neighbors in the ring using modulo
+    int left = (rank - 1 + size) % size;
+    int right = (rank + 1) % size;
 
+    myNumber = numbers[rank]; // get the number for this process
 
     // This process has no more execution to do.
     printf("Process %d has now finished.\n", rank); // Print statement as per assignment reqs.
